6e7c7b905cfd9b7800d4f56dd79d025dc8f9bf89
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-02-12 16:18:49 +0100

    Add Size

--- src/pizza.py
+++ src/pizza.py
@@ -1,7 +1,31 @@
 """
-    Module for creating a fake ingredient
+    Module for creating a fake pizza
 """
+
 import faker
+
+
+
+class Size: # pylint: disable=too-few-public-methods
+    """
+        Create a size for the pizza
+        Attributes:
+            - name
+            - ingredient_factor
+    """
+    def __init__(self, lang_code):
+        # Initialize the faker generator
+        self.fake = faker.Faker(lang_code)
+
+        # The size name
+        self.name = self.fake.word()
+
+        # The ingredient factor
+        self.ingredient_factor = self.fake.pyfloat(right_digits=1,
+                                                   positive=True,
+                                                   min_value=1,
+                                                   max_value=3)
+
 
 class Ingredient: # pylint: disable=too-few-public-methods
     """
