6b34bc91557879e75a5f28c8d6c4761d2c645f3a
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-02-13 18:16:49 +0100

    Add PaymentStatus and StatusHistory

--- OCPizza-structure.sql
+++ OCPizza-structure.sql
@@ -10,6 +10,13 @@ USE ocpizza;
 -- IT'S BASED ON THE ENTITY RELATIONSHIP DIAGRAM
 -- -----------------------------------------------------
 
+CREATE TABLE payment_status (
+                id INT AUTO_INCREMENT NOT NULL,
+                name VARCHAR(20) NOT NULL,
+                PRIMARY KEY (id)
+);
+
+
 CREATE TABLE vat_rate (
                 id INT AUTO_INCREMENT NOT NULL,
                 vat_rate_100 DECIMAL(6,4) NOT NULL,
@@ -60,12 +67,12 @@ CREATE TABLE ingredient (
 
 
 CREATE TABLE pizza (
-                pizza_id INT AUTO_INCREMENT NOT NULL,
+                id INT AUTO_INCREMENT NOT NULL,
                 name VARCHAR(50) NOT NULL,
                 tax_free_unit_price DECIMAL(10,2) NOT NULL,
                 category_id INT NOT NULL,
                 vat_rate_id INT NOT NULL,
-                PRIMARY KEY (pizza_id)
+                PRIMARY KEY (id)
 );
 
 
@@ -137,18 +144,34 @@ CREATE TABLE customer_order (
                 status_id INT NOT NULL,
                 payment_id INT NOT NULL,
                 pizzeria_id INT NOT NULL,
+                payment_status_id INT NOT NULL,
+                PRIMARY KEY (id)
+);
+
+
+CREATE TABLE status_history (
+                id INT NOT NULL,
+                status_date DATETIME NOT NULL,
+                status_id INT NOT NULL,
+                customer_order_id INT NOT NULL,
                 PRIMARY KEY (id)
 );
 
 
 CREATE TABLE order_line (
                 order_id INT NOT NULL,
-                pizza_id INT NOT NULL,
+                id INT NOT NULL,
                 size_id INT NOT NULL,
                 quantity INT NOT NULL,
-                PRIMARY KEY (order_id, pizza_id, size_id)
+                PRIMARY KEY (order_id, id, size_id)
 );
 
+
+ALTER TABLE customer_order ADD CONSTRAINT payment_status_customer_order_fk
+FOREIGN KEY (payment_status_id)
+REFERENCES payment_status (id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
 ALTER TABLE pizza ADD CONSTRAINT vat_rate_pizza_fk
 FOREIGN KEY (vat_rate_id)
@@ -180,6 +203,12 @@ REFERENCES status (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
+ALTER TABLE status_history ADD CONSTRAINT status_status_history_fk
+FOREIGN KEY (status_id)
+REFERENCES status (id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
 ALTER TABLE stock ADD CONSTRAINT ingredient_stock_fk
 FOREIGN KEY (ingredient_id)
 REFERENCES ingredient (id)
@@ -194,13 +223,13 @@ ON UPDATE NO ACTION;
 
 ALTER TABLE pizza_ingredient ADD CONSTRAINT pizza_pizza_ingredient_fk
 FOREIGN KEY (pizza_id)
-REFERENCES pizza (pizza_id)
+REFERENCES pizza (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
 ALTER TABLE order_line ADD CONSTRAINT pizza_order_line_fk
-FOREIGN KEY (pizza_id)
-REFERENCES pizza (pizza_id)
+FOREIGN KEY (id)
+REFERENCES pizza (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
@@ -230,6 +259,12 @@ ON UPDATE NO ACTION;
 
 ALTER TABLE order_line ADD CONSTRAINT order_order_line_fk
 FOREIGN KEY (order_id)
+REFERENCES customer_order (id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE status_history ADD CONSTRAINT customer_order_status_history_fk
+FOREIGN KEY (customer_order_id)
 REFERENCES customer_order (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
