1dfa3c94b424694c3f66cff2ad1613ed844fc031
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-02-17 15:35:56 +0100

    Add an email for employees with a unique index

--- OCPizza-structure.sql
+++ OCPizza-structure.sql
@@ -107,11 +107,16 @@ CREATE TABLE employee (
                 id INT AUTO_INCREMENT NOT NULL,
                 first_name VARCHAR(100) NOT NULL,
                 last_name VARCHAR(100) NOT NULL,
+                email VARCHAR(200) NOT NULL,
                 password CHAR(64) NOT NULL,
                 pizzeria_id INT NOT NULL,
                 PRIMARY KEY (id)
 );
 
+
+CREATE UNIQUE INDEX employee_email
+ ON employee
+ ( email );
 
 CREATE TABLE customer (
                 id INT AUTO_INCREMENT NOT NULL,
