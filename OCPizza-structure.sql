2206131da4babb932d05012c1f43954755ddacbc
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-02-18 18:04:49 +0100

    Add a phone number to pizzeria and customer

--- OCPizza-structure.sql
+++ OCPizza-structure.sql
@@ -122,6 +122,7 @@ CREATE TABLE pizza_ingredient (
 CREATE TABLE pizzeria (
                 id INT AUTO_INCREMENT NOT NULL,
                 name VARCHAR(50) NOT NULL,
+                phone_number VARCHAR(15) NOT NULL,
                 address_street_number VARCHAR(10) NOT NULL,
                 address_street_name VARCHAR(200) NOT NULL,
                 address_city VARCHAR(100) NOT NULL,
@@ -163,6 +164,7 @@ CREATE TABLE customer (
                 first_name VARCHAR(100) NOT NULL,
                 last_name VARCHAR(100) NOT NULL,
                 email VARCHAR(200) NOT NULL,
+                phone_number VARCHAR(15) NOT NULL,
                 password CHAR(64) NOT NULL,
                 address_street_number VARCHAR(10),
                 address_street_name VARCHAR(200),
