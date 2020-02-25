4b2a44ecdd8bb772f1c80e6524c9956a5ea69eec
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-02-25 16:33:43 +0100

    Add a request for selecting all pending orders

--- test_requests.sql
+++ test_requests.sql
@@ -117,3 +117,19 @@ LEFT JOIN pizza_ingredient ON pizza_ingredient.pizza_id = pizza.id
 LEFT JOIN ingredient ON ingredient.id = pizza_ingredient.ingredient_id
 GROUP BY ingredient.name
 ORDER BY ingredient.name;
+--
+-- Select all pending orders
+--
+SELECT
+    restaurant.name AS restaurant_name,
+    pizza.name AS pizza_name,
+    COUNT(order_line.quantity) AS pizza_quantity
+FROM order_line
+JOIN customer_order ON customer_order.id = order_line.order_id
+JOIN restaurant ON restaurant.id = customer_order.restaurant_id
+JOIN status ON status.id = customer_order.status_id
+JOIN pizza ON pizza.id = order_line.pizza_id
+WHERE status.name = 'attente'
+GROUP BY restaurant.name, pizza.name
+ORDER BY restaurant.name, pizza.name;
+
