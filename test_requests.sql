1cfb762020df52ac7a614b90ebaa20c6392fa047
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-02-25 12:31:12 +0100

    Add a request for selecting the orders from a customer with 'pending' status

--- test_requests.sql
+++ test_requests.sql
@@ -71,15 +71,31 @@ WHERE
     restaurant.name = 'Gerard' AND
     status.name = 'attente';
 --
--- Select the orders from a customer where status is 'pending'
+-- Select the delivery address for an order even if the customer have change her·his own
 --
 SELECT
     customer.email,
+    CONCAT(
+        customer.address_street_number,
+        ' ',
+        customer.address_street_name,
+        ' ',
+        customer.address_postal_code,
+        ' ',
+        customer.address_city) AS customer_current_address,
     customer_order.order_date,
+    CONCAT(
+        customer_order.address_street_number,
+        ' ',
+        customer_order.address_street_name,
+        ' ',
+        customer_order.address_postal_code,
+        ' ',
+        customer_order.address_city) AS delivery_address,
     status.name
 FROM customer_order
 JOIN customer ON customer.id = customer_order.customer_id
 JOIN status ON status.id = customer_order.status_id
 WHERE
-    customer.email = 'celina.bailly@lebrun.fr' AND
-    status.name = 'attente';
+    customer.email = 'anouk.descamps@chevallier.fr' AND
+    customer.address_street_name != customer_order.address_street_name;
